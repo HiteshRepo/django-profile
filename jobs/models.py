97ed9e54d8425e7a44c05dafbc9cf80fd991a3a6
Hitesh <hiteshkumarpattanatyak03@gmail.com>
2020-10-12 18:11:31 +0530

    Jobs and Blog up and running

--- jobs/models.py
+++ jobs/models.py
@@ -7,3 +7,8 @@ class Job(models.Model):
     title = models.CharField(max_length=50, default='Dummy Project')
     image = models.ImageField(upload_to='images/', null=True, blank=True)
     summary = models.CharField(max_length=200)
+    github = models.CharField(max_length=50, null=True, blank=True)
+    link = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
