9d2d0a057629961d8060f18f4ce9e6e465ea7434
Hitesh <hiteshkumarpattanatyak03@gmail.com>
2020-10-15 12:04:20 +0530

    strip html tags from blog summary

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.utils.html import strip_tags
 
 # Create your models here.
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
@@ -10,7 +11,10 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
 
     def summary(self):
-        return self.body[:100]
+        my_string = self.body[:100]
+        my_string = strip_tags(my_string)
+
+        return my_string
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
